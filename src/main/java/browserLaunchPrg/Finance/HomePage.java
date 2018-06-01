1bbe059e0d30f3257c44422a0ee290eccbc34c57
bhshree <bhagyashreekhadatare@gmail.com>
2018-06-01 13:30:20 +0530

    finance Project
    finance Project exicute through jenkin

---
+++ src/main/java/browserLaunchPrg/Finance/HomePage.java
@@ -0,0 +1,34 @@
+package browserLaunchPrg.Finance;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage 
+{
+	@FindBy(linkText="Sign in")WebElement signIn;
+	@FindBy(id="email")WebElement email;
+	@FindBy(id="passwd")WebElement passwd;
+	@FindBy(id="SubmitLogin")WebElement SubmitLogin;
+	@FindBy(xpath="//h1[text()='My account']")WebElement verifyAccount;
+	
+	public HomePage(WebDriver driver) 
+	{
+		PageFactory.initElements(driver,this);
+	}
+	
+	public void Login()
+	{
+		signIn.click();
+		email.sendKeys("qatest@gmail.com");
+		passwd.sendKeys("password");
+		SubmitLogin.click();
+	}
+	
+	public String VerrifyLoginPage()
+	{
+		return verifyAccount.getText();
+	}
+
+}
